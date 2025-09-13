b869de65dd2b604c78213915f57f96249c0194e3
Dev Patel <devpatel1828king@gmail.com>
2025-09-13 19:20:35 +0530

    moved one constant of time slots and weekdays in profile edit constant

--- lib/screens/profile_screen/profile_edit.dart
+++ lib/screens/profile_screen/profile_edit.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
 import 'package:image_picker/image_picker.dart';
 import 'package:therapist_app/api_services/api_service.dart';
-import 'package:therapist_app/utils/color_constants/availability_constants.dart';
+import 'package:therapist_app/utils/profile_edit_constants/availability_constants.dart';
 import 'package:therapist_app/utils/color_constants/color_constants.dart';
 import 'dart:io';
 import 'dart:convert';
